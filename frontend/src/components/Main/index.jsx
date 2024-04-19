58dce52852e4f5b27d520372e4826336e8908ec6
mourad <mourad.saidani.2022@gmail.com>
2024-04-19 15:10:06 +0100

    Home,index,Sidebar-front

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -5,28 +5,35 @@ import Home from 	"./Home"
 import Sidebar from  "./Sidebar"
 
 
-const Main = () => {
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+// const Main = () => {
+// 	const handleLogout = () => {
+// 		localStorage.removeItem("token");
+// 		window.location.reload();
+// 	};
+
+	function Main() {
+		const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+	  
+		const OpenSidebar = () => {
+		  setOpenSidebarToggle(!openSidebarToggle)
+		}
 
 	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
-				<button className={styles.white_btn} onClick={handleLogout}> Logout</button>
-			</nav>
+		// <div className={styles.main_container}>
+		// 	<nav className={styles.navbar}>
+		// 		<h1>fakebook</h1>
+		// 		<button className={styles.white_btn} onClick={handleLogout}> Logout</button>
+		// 	</nav>
 
 			<div className="grid-container">
-				<Header/>
-				<Sidebar/>
+				<Header OpenSidebar={OpenSidebar}/>
+				<Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
 				<Home/>
 
 			</div>
 
 
-		</div>
+		// </div>
 
 	);
 };
