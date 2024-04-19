94ff60a57861a9881f1fd0ee0320283cb578a54a
mourad <mourad.saidani.2022@gmail.com>
2024-04-19 11:44:41 +0100

    frontend-login-signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-        <h1>its a joke</h1>
-        </BrowserRouter>
-    </div>
-  );
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
