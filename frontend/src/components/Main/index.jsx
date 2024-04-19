cd71a9d1ffd076807fcf63a4fd13ec069ad8c2b4
mourad <mourad.saidani.2022@gmail.com>
2024-04-19 16:18:31 +0100

    change

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -18,7 +18,7 @@ const Main = () => {
     return (
         <div className={"main_container"}>
             <nav className={"navbar"}>
-                <h1>fakebook</h1>
+                <h1>fakebookabc</h1>
                 <button className={"white_btn"} onClick={handleLogout}> logout</button>
             </nav>
 
