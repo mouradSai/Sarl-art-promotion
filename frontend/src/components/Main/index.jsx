b05c93705bb0404dc4aae6346685fc191a1278be
MokraneMassi <Mokranemassi06.com>
2024-04-19 15:14:44 +0100

    push-app.css

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,34 +1,34 @@
-import {useState} from "react";
-import styles from "./styles.module.css";
+import React, { useState } from 'react'; // Importez useState de React
+import styles from "./App.css";
 import Header from 	"./Header"
-import Home from 	"./Home"
-import Sidebar from  "./Sidebar"
-
+import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 const Main = () => {
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    };
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
-	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
-				<button className={styles.white_btn} onClick={handleLogout}> logout</button>
-			</nav>
+    const OpenSidebar = () => {
+        setOpenSidebarToggle(!openSidebarToggle);
+    };
 
-			<div className="grid-container">
-				<Header/>
-				<Sidebar/>
-				<Home/>
+    return (
+        <div className={"main_container"}>
+            <nav className={"navbar"}>
+                <h1>fakebook</h1>
+                <button className={"white_btn"} onClick={handleLogout}> logout</button>
+            </nav>
 
-			</div>
-
-
-		</div>
-
-	);
+            <div className="grid-container">
+                <Header OpenSidebar={OpenSidebar}/>
+                <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+                <Home />
+            </div>
+        </div>
+    );
 };
 
 export default Main;
