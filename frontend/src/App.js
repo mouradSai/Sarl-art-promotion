927b5fc3d762807237c9c115dcd805a58d8a301f
MokraneMassi <Mokranemassi06.com>
2024-04-20 21:36:52 +0100

    front-providers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-
+import Provider from "./components/provider/provider";
 function App() {
 	const user = localStorage.getItem("token");
 
 	return (
 		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
+			{user && <Route path="/" exact element={<Main/>} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/provider" element={<Provider />}/>
+
 		</Routes>
 	);
 }
