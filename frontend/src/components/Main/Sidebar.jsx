40a178744e31cb56b0cf650e37af215a9de632f2
mourad <mourad.saidani.2022@gmail.com>
2024-04-22 14:35:25 +0100

    sidebar-css

--- frontend/src/components/Main/Sidebar.jsx
+++ frontend/src/components/Main/Sidebar.jsx
@@ -1,13 +1,15 @@
 import {React  }from 'react';
 import {BsCart3,BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsListCheck, BsMenuButtonWideFill, BsFillGearFill} from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import blackLogo from '../../assets/art-black.png';
   
 function Sidebar({ openSidebarToggle, OpenSidebar }) {
   return (
     <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""}>
       <div className='sidebar-title'>
         <div className='sidebar-brand'>
-          <BsCart3 className='icon_header'/> Sarl Art Promotion
+        <img src={blackLogo} alt='Logo' className='sidebar-logo' />
+          <div className="sidebar-name">Sarl Art Groupe</div>
         </div>
         <span className='icon close_icon' onClick={OpenSidebar}>X</span>
       </div>
@@ -15,12 +17,12 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
       <ul className='sidebar-list'>
         <li className='sidebar-list-item'>
         <Link to="/provider">
-            <BsFillArchiveFill className='icon'/> Providers
+            <BsFillArchiveFill className='icon'/> Fournisseurs
           </Link>
         </li>
         <li className='sidebar-list-item'>
         <Link to="/customer">
-            <BsFillArchiveFill className='icon'/> costumers
+            <BsFillArchiveFill className='icon'/> Clients
           </Link>
         </li>
         <li className='sidebar-list-item'>
@@ -45,7 +47,7 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
         </li>
         <li className='sidebar-list-item'>
           <a href="#settings">
-            <BsFillGearFill className='icon'/> Setting
+            <BsFillGearFill className='icon'/> Options
           </a>
         </li>
       </ul>
