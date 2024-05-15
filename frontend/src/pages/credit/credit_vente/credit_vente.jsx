520fb34beb72c9e564452e61f52a92a3e931ca93
MokraneMassi <Mokranemassi06.com>
2024-05-15 11:27:17 +0100

    yy

--- frontend/src/pages/credit/credit_vente/credit_vente.jsx
+++ frontend/src/pages/credit/credit_vente/credit_vente.jsx
@@ -123,7 +123,7 @@ function App() {
                                 <td>{credit.commande.code_commande}</td>
                                 <td>{credit.commande.client_id ? credit.commande.client_id.name : 'No client'}</td>
                                 <td>{credit.commande.totalCommande.toFixed(2)} DA</td>
-                                <td>{credit.commande.versement.toFixed(2)} DA</td>
+                                <td>{credit.commande.versement} DA</td>
                                 <td>{credit.resteAPayer.toFixed(2)} DA</td>
                                 <td>{credit.commande.modePaiement}</td>
                                 <td>
