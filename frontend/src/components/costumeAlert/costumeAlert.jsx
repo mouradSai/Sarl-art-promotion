780bb5fc6b4adfa140f0bef165aa7282ee33a215
mourad <mourad.saidani.2022@gmail.com>
2024-05-22 00:02:26 +0100

    transition

--- frontend/src/components/costumeAlert/costumeAlert.jsx
+++ frontend/src/components/costumeAlert/costumeAlert.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './alert.css';
 
 const CustomAlert = ({ message, type, onClose }) => {
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
+    setVisible(true);
     const timer = setTimeout(() => {
-      onClose();
+      setVisible(false);
+      setTimeout(onClose, 500); // Attendre que la transition se termine avant de fermer
     }, 5000);
 
-    return () => clearTimeout(timer); // Cleanup the timer if the component is unmounted
+    return () => {
+      clearTimeout(timer);
+    };
   }, [onClose]);
 
   return (
-    <div className={`custom-alert ${type}`}>
+    <div className={`custom-alert ${type} ${visible ? 'show' : 'hide'}`}>
       <p>{message}</p>
-      <button className="close-button" onClick={onClose}>&times;</button>
+      <button className="close-button" onClick={() => setVisible(false)}>&times;</button>
     </div>
   );
 };
