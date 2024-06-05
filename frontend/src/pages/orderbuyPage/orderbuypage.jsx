740cacec490da185828c76c39b3970d8bd5a3a00
MokraneMassi <Mokranemassi06.com>
2024-06-05 19:35:22 +0100

    push

--- frontend/src/pages/orderbuyPage/orderbuypage.jsx
+++ frontend/src/pages/orderbuyPage/orderbuypage.jsx
@@ -348,7 +348,7 @@ function App() {
                                     <tr key={index}>
                                         <td>{item.product_name}</td>
                                         <td>{item.quantity}</td>
-                                        <td>{item.prixUnitaire}.00 DA</td>
+                                        <td>{item.prixUnitaire.toFixed(2)} DA</td>
                                         <td>{item.totalLigne} DA</td>
                                     </tr>
                                 ))}
@@ -416,7 +416,7 @@ function App() {
                                 <tr key={index}>
                                     <td>{item.product_name}</td>
                                     <td>{item.quantity}</td>
-                                    <td>{item.prixUnitaire},00 DA</td>
+                                    <td>{item.prixUnitaire.toFixed(2)}DA</td>
                                     <td>{item.totalLigne} DA</td>
                                     <td>
                                         <button className='delete-button' onClick={() => handleDelete(index)}>Supprimer</button>
